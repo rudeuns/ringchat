8eef35e235a2584667bdc8c6c0df1199f113541f
rudeuns <rudeuns@gmail.com>
2024-08-30 19:06:12 +0900

    feat: Add database models
    #4

---
+++ backend/app/db/database.py
@@ -0,0 +1,35 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import text
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env.local")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+Session = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    expire_on_commit=False,
+    autoflush=False,
+    class_=AsyncSession,
+)
+
+Base = declarative_base()
+
+
+async def init_db():
+    async with engine.begin() as connection:
+        await connection.execute(text("CREATE EXTENSION IF NOT EXISTS vector"))
+
+
+async def get_db():
+    async with Session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
