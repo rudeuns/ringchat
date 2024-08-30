78f88091006149340f468d317ebaae9f51734899
rudeuns <rudeuns@gmail.com>
2024-08-30 20:59:29 +0900

    feat: Add database model interface for type checking

--- frontend/src/lib/interfaces.ts
+++ frontend/src/lib/interfaces.ts
@@ -1,22 +1,34 @@
 export interface FolderData {
-  folderId: number;
-  folderName: string;
+  id: number;
+  name: string;
+  chat_rooms: ChatRoomData[] | null;
 }
 
 export interface ChatRoomData {
-  roomId: number;
-  roomName: string;
-  folderId: number;
-}
-
-export interface ChatMessageData {
-  question: string;
-  answer: string;
+  id: number;
+  folder_id: number | null;
+  name: string;
+  is_favorite: boolean;
 }
 
 export interface LinkData {
+  id: number;
   url: string;
-  avgScore: number;
-  sumUsedNum: number;
-  sumBookmark: number;
+  title: string;
+  link_stat: LinkStatData;
+  selected: boolean;
+}
+
+export interface LinkStatData {
+  average_rating: number;
+  rating_count: number;
+  attached_count: number;
+  favorite_count: number;
+}
+
+export interface MessageData {
+  id: number;
+  content: string;
+  is_user_message: boolean;
+  created_at: string;
 }
