8f5af4f3f32473770a10d550adbc5e4b384b04d8
rudeuns <rudeuns@gmail.com>
2024-08-31 12:48:19 +0900

    feat: Add logout API

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -24,21 +24,29 @@ async def signup_for_access_token(
     user_info_data: schemas.UserInfoCreate, db: AsyncSession = Depends(get_db)
 ):
     # 이메일 중복 확인
-    user_info = await crud.get_user_info_by_email(db=db, email=user_info_data.email)
+    user_info = await crud.get_user_info_by_email(
+        db=db, email=user_info_data.email
+    )
     if user_info:
         response.handle_bad_request(
             detail="Email already registered.", code="EMAIL_EXISTS"
         )
 
     # 비밀번호 해싱
-    hashed_password = security.get_password_hash(password=user_info_data.password)
+    hashed_password = security.get_password_hash(
+        password=user_info_data.password
+    )
     user_info_data.password = hashed_password
 
     # 새로운 사용자 정보 추가
-    new_user_info = await crud.create_user_info(db=db, user_info_data=user_info_data)
+    new_user_info = await crud.create_user_info(
+        db=db, user_info_data=user_info_data
+    )
 
     # JWT 토큰 발급
-    access_token = security.create_access_token(data={"sub": new_user_info.email})
+    access_token = security.create_access_token(
+        data={"sub": new_user_info.email}
+    )
 
     res = response.handle_success(detail="Signup and login successful.")
     res.set_cookie(
@@ -57,13 +65,17 @@ async def login_for_access_token(
     # 사용자 정보 조회
     user_info = await crud.get_user_info_by_email(db=db, email=login_data.email)
     if not user_info:
-        response.handle_bad_request(detail="Incorrect email.", code="INVALID_EMAIL")
+        response.handle_bad_request(
+            detail="Incorrect email.", code="INVALID_EMAIL"
+        )
 
     # 비밀번호 확인
     if not security.verify_password(
         plain_password=login_data.password, hashed_password=user_info.password
     ):
-        response.handle_bad_request(detail="Incorrect password.", code="INVALID_PWD")
+        response.handle_bad_request(
+            detail="Incorrect password.", code="INVALID_PWD"
+        )
 
     # JWT 토큰 발급
     access_token = security.create_access_token(data={"sub": user_info.email})
@@ -75,4 +87,12 @@ async def login_for_access_token(
         httponly=True,
         max_age=int(COOKIE_EXPIRE_SECOND),
     )
+    return res
+
+
+@router.post("/logout")
+async def logout():
+    # 쿠키에서 토큰 삭제
+    res = response.handle_success(detail="Logout successful.")
+    res.delete_cookie(key="access_token")
     return res
