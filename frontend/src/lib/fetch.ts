64f010f818ebad9ded387837b559bc094993e9a0
rudeuns <rudeuns@gmail.com>
2024-08-31 12:37:40 +0900

    refactor: Refactor common fetch function for handling error

--- frontend/src/lib/fetch.ts
+++ frontend/src/lib/fetch.ts
@@ -1,6 +1,4 @@
-import { NextResponse } from "next/server";
-
-export async function fetchServer(path: string, options: RequestInit = {}) {
+export async function fetchClient(path: string, options: RequestInit = {}) {
   const url = `${process.env.NEXT_PUBLIC_API_URL}${path}`;
 
   const headers = new Headers({
@@ -14,16 +12,28 @@ export async function fetchServer(path: string, options: RequestInit = {}) {
       headers,
       credentials: "include",
     });
-
     return res;
   } catch (error) {
-    console.log(`Error occurred while processing API request: ${error}`);
-    return NextResponse.json(
-      {
-        detail: "Error occurred while processing API request.",
-        code: "NETWORK_ERROR",
-      },
-      { status: 500 },
-    );
+    throw error;
+  }
+}
+
+export async function fetchServer(path: string, options: RequestInit = {}) {
+  const url = `${process.env.NEXT_PUBLIC_SERVER_API_URL}${path}`;
+
+  const headers = new Headers({
+    ...options.headers,
+    "Content-Type": "application/json",
+  });
+
+  try {
+    const res = await fetch(url, {
+      ...options,
+      headers,
+      credentials: "include",
+    });
+    return res;
+  } catch (error) {
+    throw error;
   }
 }
